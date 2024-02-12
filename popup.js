c0de9421144f0cb3d05870ed2cb6f5b5ba05137a
DrainCena <josephkan3@gmail.com>
2024-02-12 13:10:45 -0500

    feat: adds nuke

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 // Function to save popup data to storage
 function saveData() {
+  // Fetch users from input
+  const usersString = document.getElementById("userList").value;
+  const usersArray = usersString.split("\n").map((item) => item.trim());
+
   // Fetch keywords from input
   const keywordsString = document.getElementById("keywordList").value;
   const keywordsArray = keywordsString.split("\n").map((item) => item.trim());
@@ -17,6 +21,7 @@ function saveData() {
 
   // Save the data using the Chrome storage API
   chrome.storage.local.set({
+    hiddenUsers: usersArray,
     hiddenKeywords: keywordsArray,
     hiddenSubreddits: subredditsArray,
     loggingEnabled: loggingPreference,
@@ -26,8 +31,13 @@ function saveData() {
 function loadData() {
   // Load the data using the Chrome storage API
   chrome.storage.local.get(
-    ["hiddenKeywords", "hiddenSubreddits", "loggingEnabled"],
+    ["hiddenUsers", "hiddenKeywords", "hiddenSubreddits", "loggingEnabled"],
     function (result) {
+      if (result.hiddenUsers) {
+        document.getElementById("userList").value =
+          result.hiddenUsers.join("\n");
+      }
+
       if (result.hiddenKeywords) {
         document.getElementById("keywordList").value =
           result.hiddenKeywords.join("\n");
@@ -46,11 +56,49 @@ function loadData() {
   );
 }
 
+function nuke() {
+  // Sends request to main content script for users to ban
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "nukePage" },
+      function (response = []) {
+        // Adds all found users to the ban list
+        const usersString = document.getElementById("userList").value;
+        const usersArray = usersString.split("\n").map((item) => item.trim());
+        const combinedUsersArray = [...response, ...usersArray];
+
+        // Save combined array
+        chrome.storage.local.set({
+          hiddenUsers: combinedUsersArray,
+        });
+
+        // Print combined array to UI
+        document.getElementById("userList").value =
+          combinedUsersArray.join("\n");
+      }
+    );
+  });
+}
+
 // Set up event listeners on the textarea elements for the input event
+document.getElementById("userList").addEventListener("input", saveData);
 document.getElementById("keywordList").addEventListener("input", saveData);
 document.getElementById("subredditList").addEventListener("input", saveData);
 document
   .getElementById("loggingPreference")
   .addEventListener("change", saveData);
 
+// Loads nuke button listener
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.querySelector(".nukeButton");
+
+  if (button) {
+    button.addEventListener("click", function () {
+      nuke();
+    });
+  }
+});
+
+// Loads saved data back into input
 document.addEventListener("DOMContentLoaded", loadData);
---
+++ redditFilters.js
@@ -0,0 +1,310 @@
+// Handles the two website designs
+const oldReddit = window.location.hostname === "old.reddit.com";
+
+function showImages() {
+  // Handle old reddit design
+  if (oldReddit) {
+    const links = document.querySelectorAll("a");
+
+    links.forEach((link) => {
+      const href = link.href;
+
+      // Check if the href points to an image (basic check)
+      if (link.textContent.trim() === "<image>") {
+        const img = document.createElement("img");
+        img.src = href;
+        img.style.maxWidth = "100%"; // Optional: to ensure large images don't overflow
+
+        // Replace the link with the image
+        link.parentNode.replaceChild(img, link);
+      }
+    });
+    // New reddit design automatically fully displays
+  } else {
+  }
+}
+
+function banPosts(subreddits, keywords, users) {
+  // Do not ban posts of a dedicated thread page
+  if (window.location.pathname.includes("/comments/")) return;
+  // Handle old reddit design
+  if (oldReddit) {
+    const posts = document.querySelectorAll(".thing:not(.promotedlink)");
+    const visiblePosts = Array.from(posts).filter(
+      (el) => window.getComputedStyle(el).display !== "none"
+    ); // Ignores hidden posts to avoid O(n^2) due to constant dom changes
+
+    visiblePosts.forEach((post) => {
+      const subreddit = post.getAttribute("data-subreddit");
+      const title = post.querySelectorAll(`a.title`)[0].innerHTML;
+      const author = post.getAttribute("data-author");
+      if (subreddit && title) {
+        // Ban subreddits
+        if (subreddits.includes(subreddit)) {
+          if (loggingEnabled)
+            console.log(
+              `Hiding post based on subreddit: ${subreddit}: ${title}`
+            );
+          post.style.display = "none";
+          return;
+        }
+
+        // Ban keywords
+        lowerTitle = title.toLowerCase();
+        for (let banWord of keywords) {
+          if (lowerTitle.includes(banWord)) {
+            if (loggingEnabled)
+              console.log(`Hiding post based on keyword: ${banWord}: ${title}`);
+            post.style.display = "none";
+            return;
+          }
+        }
+
+        // Ban users
+        if (users.includes(author)) {
+          if (loggingEnabled)
+            console.log(`Hiding post based on user: ${author}: ${title}`);
+          post.style.display = "none";
+          return;
+        }
+      }
+    });
+    // Handle new reddit design
+  } else {
+    const posts = document.querySelectorAll("shreddit-post");
+    const visiblePosts = Array.from(posts).filter(
+      (el) => window.getComputedStyle(el).display !== "none"
+    ); // Ignores hidden posts to avoid O(n^2) due to constant dom changes
+
+    visiblePosts.forEach((post) => {
+      const subreddit = post.getAttribute("subreddit-prefixed-name").slice(2); // Slices off r/ prefix from subreddit name
+      const title = post.getAttribute("post-title");
+      const author = post.getAttribute("author");
+      // Ban subreddits
+      if (subreddits.includes(subreddit)) {
+        if (loggingEnabled)
+          console.log(`Hiding post based on subreddit: ${subreddit}: ${title}`);
+        post.style.display = "none";
+        return;
+      }
+
+      // Ban keywords
+      lowerTitle = title.toLowerCase();
+      for (let banWord of keywords) {
+        if (lowerTitle.includes(banWord)) {
+          if (loggingEnabled)
+            console.log(`Hiding post based on keyword: ${banWord}: ${title}`);
+          post.style.display = "none";
+          return;
+        }
+      }
+
+      // Ban users
+      if (users.includes(author)) {
+        if (loggingEnabled)
+          console.log(`Hiding post based on user: ${author}: ${title}`);
+        post.style.display = "none";
+        return;
+      }
+    });
+  }
+}
+
+function banComments(users = []) {
+  if (oldReddit) {
+    const comments = document
+      .querySelector(".commentarea")
+      .querySelectorAll(".thing:not(.morechildren,.deleted)");
+    const visibleComments = Array.from(comments).filter(
+      (el) => window.getComputedStyle(el).display !== "none"
+    ); // Ignores hidden posts to avoid O(n^2) due to constant dom changes
+
+    visibleComments.forEach((comment) => {
+      const author = comment.getAttribute("data-author");
+      if (author && users.includes(author)) {
+        // Ban users
+        if (loggingEnabled)
+          console.log(`Hiding comment based on user: ${author}`);
+        comment.style.display = "none";
+        return;
+      }
+    });
+    // Handle new reddit design
+  } else {
+    const comments = document.querySelectorAll("shreddit-comment");
+    const visibleComments = Array.from(comments).filter(
+      (el) => window.getComputedStyle(el).display !== "none"
+    ); // Ignores hidden posts to avoid O(n^2) due to constant dom changes
+
+    visibleComments.forEach((comment) => {
+      const author = comment.getAttribute("author");
+      if (author && users.includes(author)) {
+        // Ban users
+        if (loggingEnabled)
+          console.log(`Hiding comment based on user: ${author}`);
+        comment.style.display = "none";
+        return;
+      }
+    });
+  }
+}
+
+function getSavedOptions() {
+  chrome.storage.local.get(
+    ["hiddenUsers", "hiddenKeywords", "hiddenSubreddits", "loggingEnabled"],
+    function (result) {
+      if (result.hiddenUsers) {
+        for (user of result.hiddenUsers) {
+          // Slices off u/ in case the user included it
+          let cleanedUser = user;
+          if (cleanedUser.length >= 2) {
+            if (user.substring(0, 2) == "r/") {
+              cleanedUser == user.slice(2);
+            }
+          }
+          user_bans.push(cleanedUser);
+        }
+      }
+
+      if (result.hiddenKeywords) {
+        for (keyword of result.hiddenKeywords) {
+          if (keyword.trim() != "") {
+            keyword_bans.push(keyword.toLowerCase());
+          }
+        }
+      }
+
+      if (result.hiddenSubreddits) {
+        for (subreddit of result.hiddenSubreddits) {
+          // Slices off r/ in case the user included it
+          let cleanedSubreddit = subreddit;
+          if (subreddit.length >= 2) {
+            if (subreddit.substring(0, 2) == "r/") {
+              cleanedSubreddit == subreddit.slice(2);
+            }
+          }
+          subreddit_bans.push(cleanedSubreddit.toLowerCase());
+        }
+      }
+
+      if (result.loggingEnabled !== undefined) {
+        loggingEnabled = result.loggingEnabled;
+      }
+    }
+  );
+}
+
+let user_bans = [];
+let subreddit_bans = [];
+let keyword_bans = [];
+let loggingEnabled = true;
+
+// Run the function when the DOM is fully loaded
+function observeDOMChanges() {
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.addedNodes.length) {
+        getSavedOptions();
+        banPosts(subreddit_bans, keyword_bans, user_bans);
+        banComments(user_bans);
+        showImages();
+      }
+    });
+  });
+
+  var config = { childList: true, subtree: true };
+  observer.observe(document.body, config);
+}
+
+function nukeAnimation() {
+  // Create the overlay div
+  const overlay = document.createElement("div");
+  overlay.id = "myExtensionOverlay";
+
+  // Create the img element for the GIF
+  const gif = document.createElement("img");
+  gif.src = chrome.runtime.getURL("assets/explosion-boom.gif"); // Replace with the actual URL of your GIF
+  overlay.appendChild(gif);
+
+  // Append the overlay to the body
+  document.body.appendChild(overlay);
+
+  overlay.style.cssText = `
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+  gif.style.cssText = `
+  width: 100vw;
+  height: auto;
+`;
+
+  // Remove the overlay after 4.5 seconds
+  setTimeout(() => {
+    overlay.remove();
+  }, 4500);
+}
+
+function handleNukeRequest(request, sender, sendResponse) {
+  if (request.action === "nukePage") {
+    console.log("Kablooie!");
+    nukeAnimation();
+    let usersToBan = [];
+    if (oldReddit) {
+      const postsAndComments = document.querySelectorAll(
+        ".thing:not(.morechildren,.deleted)"
+      );
+
+      // Fetches all authors in the thread and returns them
+      usersToBan = Array.from(postsAndComments).reduce(
+        (accumulatedBans, comment) => {
+          const author = comment.getAttribute("data-author");
+          // Only ban users that aren't already banned
+          if (author && !user_bans.includes(author)) {
+            accumulatedBans.push(author);
+          }
+          return accumulatedBans;
+        },
+        []
+      );
+      sendResponse(usersToBan);
+      // Handle new reddit design
+    } else {
+      const postsAndComments = document.querySelectorAll(
+        "shreddit-post, shreddit-comment"
+      );
+
+      // Fetches all authors in the thread and returns them
+      usersToBan = Array.from(postsAndComments).reduce(
+        (accumulatedBans, comment) => {
+          const author = comment.getAttribute("author");
+          // Only ban users that aren't already banned
+          if (author && !user_bans.includes(author)) {
+            accumulatedBans.push(author);
+          }
+          return accumulatedBans;
+        },
+        []
+      );
+      sendResponse(usersToBan);
+    }
+    banComments(usersToBan);
+  }
+}
+
+// Listen for nuke request
+chrome.runtime.onMessage.addListener(handleNukeRequest);
+
+// Start observing and hide existing elements
+getSavedOptions();
+showImages();
+banComments(user_bans);
+banPosts(subreddit_bans, keyword_bans, user_bans);
+observeDOMChanges();
